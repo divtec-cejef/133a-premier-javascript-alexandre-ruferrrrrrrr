48022497cdc3d004863ae2040e58b8a32a631693
rufer alexandre <alexandre.rufer@divtec.ch>
2023-08-29 16:20:44 +0200

    ajout  du javascript dans la liste

--- js/main.js
+++ js/main.js
@@ -10,7 +10,22 @@
 let tabPara = document.querySelector("p");
 
 //Récupérer le premier bouton avec la classe ajouter
-let btAjouter = document.querySelector("button.ajouter");
+let btAjouter = document.querySelector(".ajouter");
+
+//Récupérer le champ article
+let txtArticle = document.querySelector("#article");
+
+//Récupère la première liste à puce avec la classe articles
+let ulArticles = document.querySelector("ul.articles");
 
 //Affiche les contenues
-console.log(tabPara, btAjouter);
+console.log(tabPara, btAjouter, txtArticle, ulArticles);
+
+//Ecoute le clique sur le bouton ajouter
+btAjouter.addEventListener("click", () => {
+    ulArticles.innerHTML += "<li>" + txtArticle.value + "</li>";
+    //vide le champ de texte
+    txtArticle.value = "";
+});
+
+
